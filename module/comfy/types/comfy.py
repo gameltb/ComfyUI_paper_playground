c19a212e98302d084dd87c48213f29b2dd94e168
gameltb <gamegccltb@gmail.com>
2024-06-20 15:15:05 +0800

    update type annotate

--- module/comfy/types/comfy.py
+++ module/comfy/types/comfy.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Any, Callable, ClassVar, List, Literal, Mapping, Optional, Union
+from typing import Annotated, Any, Callable, ClassVar, List, Literal, Mapping, Optional, TypedDict, Union
 
 import comfy.clip_vision
 import comfy.controlnet
@@ -10,13 +10,23 @@ from pydantic import BaseModel
 from .common import gen_widget
 
 MaskType = Annotated[torch.Tensor, gen_widget("MASK")]
-"""Tensor [B,H,W]"""
+"""Tensor [B,H,W] 0~1"""
 
 ImageType = Annotated[torch.Tensor, gen_widget("IMAGE")]
-"""Tensor [B,H,W,C] float32 cpu"""
+"""Tensor [B,H,W,C] 0~1"""
 
-LatentType = Annotated[dict[str, torch.Tensor], gen_widget("LATENT")]
-"""samples : Tensor [B,H,W,C]"""
+
+class LatentTypeDict(TypedDict, total=False):
+    samples: torch.Tensor
+    """[B,C,H,W] 0~1"""
+    batch_index: torch.Tensor
+    """[B]"""
+    noise_mask: torch.Tensor
+    """[B,C,H,W] 0~1"""
+
+
+LatentType = Annotated[LatentTypeDict, gen_widget("LATENT")]
+"""samples batch_index noise_mask"""
 
 VaeType = Annotated[comfy.sd.VAE, gen_widget("VAE")]
 
@@ -26,7 +36,24 @@ SigmasType = Annotated[torch.Tensor, gen_widget("SIGMAS")]
 
 ClipType = Annotated[comfy.sd.CLIP, gen_widget("CLIP")]
 
-ConditioningType = Annotated[list[tuple[torch.Tensor, dict[str, torch.Tensor]]], gen_widget("CONDITIONING")]
+
+class ConditioningAttrTypeDict(TypedDict, total=False):
+    pooled_output: torch.Tensor
+    area: Union[tuple[int, int, int, int], tuple[str, int, int, int, int]]
+    strength: float
+    set_area_to_bounds: bool
+    mask: MaskType
+    mask_strength: float
+    start_percent: float
+    end_percent: float
+    concat_latent_image: torch.Tensor
+    """[B,C,H,W] 0~1"""
+    concat_mask: MaskType
+    prompt_type: str
+
+
+ConditioningType = Annotated[list[tuple[torch.Tensor, ConditioningAttrTypeDict]], gen_widget("CONDITIONING")]
+"""list of [cond, cond_attr_dict] """
 
 ClipVisionType = Annotated[comfy.clip_vision.ClipVisionModel, gen_widget("CLIP_VISION")]
 
